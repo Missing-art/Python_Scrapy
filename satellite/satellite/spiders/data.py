4031bea63a6687fa2e137e5478c3f788caca64f8
zhangliang <1617851696@qq.com>
2021-10-24 20:16:06 +0800

    设置循环遍历

--- satellite/satellite/spiders/data.py
+++ satellite/satellite/spiders/data.py
@@ -3,8 +3,22 @@ import scrapy
 
 class DataSpider(scrapy.Spider):
     name = 'data'
-    allowed_domains = ['www.xxxxx.com']
-    start_urls = ['http://www.xxxxx.com/']
-
+    #allowed_domains = ['www.xxxxx.com']
+    start_urls = ['file:///H:/GNSS数据处理/刘正/北斗星通/第一批/225/华测自动化检测报告-GPS.html']
+    #定义一个通用的url模板
+    url = 'file:///H:/GNSS数据处理/刘正/北斗星通/第一批/%d/华测自动化检测报告-%s.html'
+    num = 255
     def parse(self, response):
-        pass
+        # 这里要注意一个小细节，xpath是没有办法访问tbody的，所以我们要在tbody的部分使用 // 直接进入tbody内部进行数据爬取
+        div_list = response.xpath('/html/body/div/div/div')
+        check = div_list[1].xpath('./table//tr[3]/td[2]/text()').extract_first()
+        print(check)
+    
+            
+
+               
+
+            
+
+            
+        
